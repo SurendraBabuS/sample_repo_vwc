408dfe9725c944cf9f2f1866f3e46b91966121b4
SurendraBabuS <118732171+SurendraBabuS@users.noreply.github.com>
2022-11-23 17:32:16 +0530

    File Uploaded

---
+++ TestController.java
@@ -0,0 +1,17 @@
+package com.sb.practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/")
+	public String hello() {
+		
+		return "Hello Controller";
+	}
+
+
+}
